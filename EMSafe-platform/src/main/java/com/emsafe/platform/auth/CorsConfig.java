9e1578237ce06988d132ce4fd09d4eb516e00845
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-07-02 19:46:07 -0500

    Add files via upload

---
+++ EMSafe-platform/src/main/java/com/emsafe/platform/auth/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.emsafe.platform.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200") // Puerto de Angular
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
