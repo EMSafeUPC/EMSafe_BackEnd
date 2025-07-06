964bd8225bf22d675984ad6b35eb09975185597d
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-07-06 14:37:37 -0500

    feat: update CorsConfig.java

--- EMSafe-platform/src/main/java/com/emsafe/platform/auth/CorsConfig.java
+++ EMSafe-platform/src/main/java/com/emsafe/platform/auth/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200") // Puerto de Angular
+                .allowedOrigins(
+                        "http://localhost:4200",               // Para desarrollo local
+                        "https://emsafe-bcdfd.web.app"         // Para frontend en Firebase
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
