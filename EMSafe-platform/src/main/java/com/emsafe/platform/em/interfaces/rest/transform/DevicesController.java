0b4facc19700f3f1f60bdfa556f5ee30e4927112
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-07-06 19:52:06 -0500

    Update DevicesController.java

--- EMSafe-platform/src/main/java/com/emsafe/platform/em/interfaces/rest/transform/DevicesController.java
+++ EMSafe-platform/src/main/java/com/emsafe/platform/em/interfaces/rest/transform/DevicesController.java
@@ -20,7 +20,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping(value = "/api/v1/devices", produces = APPLICATION_JSON_VALUE)
 @CrossOrigin(
-        origins = { "https://emsafe.netlify.app", "http://localhost:4200" },
+        origins = "*",
         allowedHeaders = "*",
         methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
                 RequestMethod.DELETE, RequestMethod.OPTIONS }
