c80f8c4817e0d92654464b6bbe03d18f7a720a15
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-07-06 19:52:19 -0500

    Update MapController.java

--- EMSafe-platform/src/main/java/com/emsafe/platform/em/interfaces/rest/transform/MapController.java
+++ EMSafe-platform/src/main/java/com/emsafe/platform/em/interfaces/rest/transform/MapController.java
@@ -16,7 +16,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(value = "/api/v1/map", produces = APPLICATION_JSON_VALUE)
-@CrossOrigin(origins = {"https://emsafe.netlify.app", "http://localhost:4200"})
+@CrossOrigin(origins = "*")
 @Tag(name = "Radiation Map", description = "Endpoints para gestión de puntos de radiación")
 public class MapController {
 
